2ce921e389478669fc56356d2ad23eaa41e68605
joão paulo <jpmfbc@gmail.com>
2019-02-11 11:30:29 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/joaopaulo/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/joaopaulo/cursomc/repositories/PedidoRepository.java
@@ -1,9 +1,15 @@
 package com.joaopaulo.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.joaopaulo.cursomc.domain.Cliente;
 import com.joaopaulo.cursomc.domain.Pedido;
 
 public interface PedidoRepository extends JpaRepository<Pedido,Integer> {
 
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente,Pageable pageRequest);
 }
